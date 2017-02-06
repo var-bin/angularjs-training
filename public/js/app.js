825a2d1d7a22e82ab881dae98777a93ee8fc92a0
var-bin <var-bin@users.noreply.github.com>
2017-02-06 17:30:47 +0200

    Add new item

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,14 @@
       name: "Test gem 1",
       price: 2.98,
       description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 1111",
-      canPurchase: true,
+      canPurchase: false,
+      soldOut: false
+    },
+    {
+      name: "Test gem 2",
+      price: 5.3,
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. 2222",
+      canPurchase: false,
       soldOut: false
     }
   ]
