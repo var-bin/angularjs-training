53377c9966874f0d3993bd24c73f9177da1777cc
var-bin <var-bin@users.noreply.github.com>
2017-02-09 18:32:37 +0200

    Refactoring. Replace code from custom Controllers to custom Directives

--- public/js/app.js
+++ public/js/app.js
@@ -6,42 +6,6 @@
     this.product = gems;
   });
 
-  app.controller("TabController", function () {
-    this.tab = 1;
-
-    this.setTab = function (setTab) {
-      this.tab = setTab;
-    };
-
-    this.isSet = function (setValue) {
-      return this.tab === setValue;
-    };
-
-    this.setActive = function (activeValue) {
-      return this.tab === activeValue;
-    };
-  });
-
-  app.controller("GalleryController", function() {
-    this.currentImage = 0;
-
-    this.setCurrent = function (currentValue) {
-      this.currentImage = currentValue || 0;
-    };
-
-    this.toggleCurrent = function (toggleValue) {
-      if (this.currentImage === toggleValue) {
-        return false;
-      }
-
-      this.currentImage = toggleValue || 0;
-    };
-
-    this.setActive = function (activeValue) {
-      return this.currentImage === activeValue;
-    };
-  });
-
   app.controller("ReviewController", function () {
     this.review = {};
 
@@ -51,6 +15,63 @@
 
       this.review = {};
     };
+  });
+
+  app.directive("productTitle", function () {
+    return {
+      restrict: "A",
+      templateUrl: "product-title.html"
+    }
+  });
+
+  app.directive("productPanels", function () {
+    return {
+      restrict: "E",
+      templateUrl: "product-panels.html",
+      controller: function () {
+        this.tab = 1;
+
+        this.setTab = function (setTab) {
+          this.tab = setTab;
+        };
+
+        this.isSet = function (setValue) {
+          return this.tab === setValue;
+        };
+
+        this.setActive = function (activeValue) {
+          return this.tab === activeValue;
+        };
+      },
+      controllerAs: "navTab"
+    }
+  });
+
+  app.directive("productGallery", function () {
+    return {
+      restrict: "E",
+      templateUrl: "product-gallery.html",
+      controller: function () {
+        this.currentImage = 0;
+
+        this.setCurrent = function (currentValue) {
+          this.currentImage = currentValue || 0;
+        };
+
+        this.toggleCurrent = function (toggleValue) {
+          if (this.currentImage === toggleValue) {
+            return false;
+          }
+
+          this.currentImage = toggleValue || 0;
+        };
+
+        this.setActive = function (activeValue) {
+          return this.currentImage === activeValue;
+        };
+      },
+      controllerAs: "gallery"
+    }
   });
 
   var gems = [
