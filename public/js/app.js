165a165719b68050b7582d58fddcfb9703e3b82f
var-bin <var-bin@users.noreply.github.com>
2017-02-10 19:27:14 +0200

    Refactoring. Use Modules and Dependencies

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 "use strict";
+
 (function() {
-  var app = angular.module("gemStore", []);
+  var app = angular.module("gemStore", ["storeProducts"]);
 
   app.controller("StoreController", function () {
     this.product = gems;
@@ -14,70 +15,6 @@
       product.reviews.push(this.review);
 
       this.review = {};
-    };
-  });
-
-  app.directive("productTitle", function () {
-    return {
-      restrict: "A",
-      templateUrl: "product-title.html"
-    }
-  });
-
-  app.directive("productPanels", function () {
-    return {
-      restrict: "E",
-      templateUrl: "product-panels.html",
-      controller: function () {
-        this.tab = 1;
-
-        this.setTab = function (setTab) {
-          this.tab = setTab;
-        };
-
-        this.isSet = function (setValue) {
-          return this.tab === setValue;
-        };
-
-        this.setActive = function (activeValue) {
-          return this.tab === activeValue;
-        };
-      },
-      controllerAs: "navTab"
-    }
-  });
-
-  app.directive("productGallery", function () {
-    return {
-      restrict: "E",
-      templateUrl: "product-gallery.html",
-      controller: function () {
-        this.currentImage = 0;
-
-        this.setCurrent = function (currentValue) {
-          this.currentImage = currentValue || 0;
-        };
-
-        this.toggleCurrent = function (toggleValue) {
-          if (this.currentImage === toggleValue) {
-            return false;
-          }
-
-          this.currentImage = toggleValue || 0;
-        };
-
-        this.setActive = function (activeValue) {
-          return this.currentImage === activeValue;
-        };
-      },
-      controllerAs: "gallery"
-    }
-  });
-
-  app.directive("productButton", function () {
-    return {
-      restrict: "E",
-      templateUrl: "product-button.html"
     };
   });
 
