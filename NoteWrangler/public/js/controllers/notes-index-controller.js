90b773cb223779a09a09b55f75262b859fead11b
var-bin <var-bin@users.noreply.github.com>
2017-02-17 15:20:34 +0200

    Define NotesIndexController

--- NoteWrangler/public/js/controllers/notes-index-controller.js
+++ NoteWrangler/public/js/controllers/notes-index-controller.js
@@ -1,1 +1,17 @@
 // js/controllers/notes-index-controller
+
+(function() {
+  "use strict";
+
+  angular.module("NoteWrangler")
+    .controller("NotesIndexController", function ($http) {
+      let controller = this;
+
+      $http({
+        method: "GET",
+        url: "/api/notes"
+      }).success( (data) => {
+        controller.notes = data;
+      });
+    });
+})();
