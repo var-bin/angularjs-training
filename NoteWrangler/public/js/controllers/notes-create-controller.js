2b4ac08bb887403924c917cc2e84b53e10c1414a
var-bin <var-bin@users.noreply.github.com>
2017-02-17 15:19:48 +0200

    Wrapped all code to function.

--- NoteWrangler/public/js/controllers/notes-create-controller.js
+++ NoteWrangler/public/js/controllers/notes-create-controller.js
@@ -1,1 +1,25 @@
 // js/controllers/notes-create-controller
+
+(function() {
+  "use strict";
+
+  angular.module("NoteWrangler")
+    .controller("NotesCreateController", ($http) => {
+      let controller = this;
+
+      this.saveNote = saveNote;
+
+      function saveNote(note) {
+        controller.errors = {};
+
+        return $http({
+          method: "POST",
+          url: "/api/notes",
+          data: note
+        }).catch( (note) => {
+          controller.errors = note.data.error;
+        });
+      }
+    });
+})();
+
