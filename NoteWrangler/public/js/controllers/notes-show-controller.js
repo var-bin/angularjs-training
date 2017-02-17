0fa674997b0aa4828574ac095129f4610e8bb734
var-bin <var-bin@users.noreply.github.com>
2017-02-17 18:10:07 +0200

    Add logic for NotesShowController

--- NoteWrangler/public/js/controllers/notes-show-controller.js
+++ NoteWrangler/public/js/controllers/notes-show-controller.js
@@ -1,1 +1,25 @@
 // js/controllers/notes-show-controller
+
+(function() {
+  "use strict";
+
+  angular.module("NoteWrangler")
+    .controller("NotesShowController", NotesShowController);
+
+  function NotesShowController($http, $routeParams) {
+    let vm = this;
+
+    vm.showNote = showNote;
+
+    showNote();
+
+    function showNote(id) {
+      $http({
+        method: "GET",
+        url: "/api/notes/" + $routeParams.id
+      }).success( (noteData) => {
+        vm.note = noteData;
+      });
+    }
+  }
+})();
