c92a829745dc114c767899fb51a384bea41819e0
var-bin <var-bin@users.noreply.github.com>
2017-02-21 14:48:08 +0200

    Refactoring. Replace Navbar to Directive. Create Contoller andd template

---
+++ NoteWrangler/public/js/directives/navbar.js
@@ -0,0 +1,17 @@
+// js/directives/navbar
+
+(function() {
+  "use strict";
+
+  angular.module("NoteWrangler")
+    .directive("navbar", [navbarDirective]);
+
+  function navbarDirective() {
+    return {
+      restrict: 'E',
+      templateUrl: '../templates/directives/navbar.html',
+      controller: "NavbarController",
+      controllerAs: "NavbarCtrl"
+    }
+  }
+})();
