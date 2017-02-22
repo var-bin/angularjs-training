0c2360fa07b8b13b1f49e3c88c024dc8421cef68
var-bin <var-bin@users.noreply.github.com>
2017-02-22 19:01:52 +0200

    Refactoring. Use  to id

--- NoteWrangler/public/js/controllers/navbar-controller.js
+++ NoteWrangler/public/js/controllers/navbar-controller.js
@@ -6,15 +6,19 @@
   angular.module("NoteWrangler")
     .controller("NavbarController", NavbarController);
 
-  NavbarController.$inject = ["$location"];
+  NavbarController.$inject = ["$location", "$routeParams"];
 
-  function NavbarController($location) {
+  function NavbarController($location, $routeParams) {
     let vm = this;
 
     vm.setActive = setActive;
 
     function setActive(path) {
-      return $location.path() == path;
+      if (!$routeParams.id) {
+        return $location.path() == path;
+      }
+
+      return $location.path() == path + "/" + $routeParams.id;
     }
   }
 })();
