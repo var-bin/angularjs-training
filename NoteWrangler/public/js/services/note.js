7c9b2465c6abc27332390ec2a93190e33763130b
var-bin <var-bin@users.noreply.github.com>
2017-02-23 19:42:20 +0200

    Refactoring. Use ngResource API

--- NoteWrangler/public/js/services/note.js
+++ NoteWrangler/public/js/services/note.js
@@ -6,35 +6,9 @@
   angular.module("NoteWrangler")
     .factory("Note", NoteFactory);
 
-  NoteFactory.$inject = ["$http", "$routeParams"];
+  NoteFactory.$inject = ["$routeParams", "$resource"];
 
-  function NoteFactory($http, $routeParams) {
-    function getAllNotes() {
-      return $http({
-        method: "GET",
-        url: "/api/notes"
-      });
-    }
-
-    function getNoteById() {
-      return $http({
-        method: "GET",
-        url: "/api/notes/" + $routeParams.id
-      })
-    }
-
-    function createNote(noteData) {
-      return $http({
-        method: "POST",
-        url: "/api/notes",
-        data: noteData
-      })
-    }
-
-    return {
-      getAllNotes,
-      createNote,
-      getNoteById
-    }
+  function NoteFactory($routeParams, $resource) {
+    return $resource("/api/notes/:id", {id: "@id"});
   }
 })();
