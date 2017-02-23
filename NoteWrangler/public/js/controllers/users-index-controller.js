0a685a99be4515a67de944290258f722d6f2c9c7
var-bin <var-bin@users.noreply.github.com>
2017-02-23 13:02:15 +0200

    Add logic for index Controller

--- NoteWrangler/public/js/controllers/users-index-controller.js
+++ NoteWrangler/public/js/controllers/users-index-controller.js
@@ -1,1 +1,30 @@
 // js/controllers/users-index-controller
+
+(function() {
+  "use strict";
+
+  angular.module("NoteWrangler")
+    .controller("UsersIndexController", UsersIndexController);
+
+  UsersIndexController.$inject = ["User"];
+
+  function UsersIndexController(User) {
+    let vm = this;
+
+    vm.getAvatar = getAvatar;
+
+    getAllUsers();
+
+    function getAllUsers() {
+      User.getAllUsers()
+      .success( (usersData) => {
+        vm.users = usersData;
+      });
+    }
+
+    function getAvatar(email) {
+      return Gravatar.getAvatar(email);
+    }
+  }
+
+})();
