33e9a782dc3c2b00adfe68a3cc23fbfc12629bcc
var-bin <var-bin@users.noreply.github.com>
2017-02-23 17:00:52 +0200

    Add config for GravatarProvider

--- NoteWrangler/public/js/app.js
+++ NoteWrangler/public/js/app.js
@@ -3,5 +3,13 @@
 (function() {
   "use strict";
 
-  angular.module("NoteWrangler", ["ngRoute"]);
+  angular.module("NoteWrangler", ["ngRoute"])
+    .config(config);
+
+  config.$inject = ["GravatarProvider"];
+
+  function config(GravatarProvider) {
+    GravatarProvider.setSize(60);
+    GravatarProvider.setDefaultImage("monsterid");
+  }
 })();
