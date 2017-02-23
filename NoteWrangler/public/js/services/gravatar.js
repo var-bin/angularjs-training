d915031aaab1671934cc20f31ca19fb7d2b9122a
var-bin <var-bin@users.noreply.github.com>
2017-02-23 13:03:09 +0200

    Add logic for gravatar Service

--- NoteWrangler/public/js/services/gravatar.js
+++ NoteWrangler/public/js/services/gravatar.js
@@ -3,4 +3,23 @@
 (function() {
   "use strict";
 
+  //import { MD5 } from "crypto-js/md5";
+
+  angular.module("NoteWrangler")
+    .factory("Gravatar", GravatarFactory);
+
+  GravatarFactory.$inject = ["$http"];
+
+  function GravatarFactory($http) {
+    const AVATAR_SIZE = 80;
+    const AVATAR_URL = "http://www.gravatar.com/avatar/";
+
+    function getAvatar(email) {
+      //return AVATAR_URL + MD5(email) + "?size=" + AVATAR_SIZE.toString();
+    }
+
+    return {
+      getAvatar
+    }
+  }
 })();
