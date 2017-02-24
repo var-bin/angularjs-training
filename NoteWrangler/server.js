c32276cc0fb3de6e9ef95b46636d6c36d3393fed
var-bin <var-bin@users.noreply.github.com>
2017-02-24 18:42:32 +0200

    Add new constants for NODE_ENV, indexHtml. Edit default route

--- NoteWrangler/server.js
+++ NoteWrangler/server.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults({
   static: path.join(__dirname, "app")
 });
@@ -14,6 +14,14 @@ const middlewares = jsonServer.defaults({
 const PORT = 3000;
 const HOST_NAME = "localhost";
 const HOST = `${HOST_NAME}:${PORT}`;
+
+const NODE_ENV_DEVELOPMENT = "development";
+const NODE_ENV_PRODUCTION = "production";
+
+const INDEX_DEV = path.join(__dirname, "index.html");
+const INDEX_PROD = path.join(__dirname, "index.min.html");
+
+let indexHtml = INDEX_DEV;
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
@@ -25,9 +33,17 @@ server.use(middlewares);
 // Use default router
 server.use("/api", router);
 
+server.use( (req, res, next) => {
+  if (process.env.NODE_ENV != NODE_ENV_DEVELOPMENT) {
+    indexHtml = INDEX_PROD;
+  }
+
+  next();
+});
+
 // Add custom routes before JSON Server router
 server.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(indexHtml);
 })
 
 server.listen(PORT, () => {
