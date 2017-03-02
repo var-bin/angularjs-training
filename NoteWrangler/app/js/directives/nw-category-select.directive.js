da17e9bd5480a26c4fbb91ec2a44739cec651319
var-bin <var-bin@users.noreply.github.com>
2017-03-02 17:54:53 +0200

    Edit templateUrl value. Add require, link values. Add nwCategoryItemDirective.. Comment code

--- NoteWrangler/app/js/directives/nw-category-select.directive.js
+++ NoteWrangler/app/js/directives/nw-category-select.directive.js
@@ -6,12 +6,30 @@
   angular.module("NoteWrangler")
     .directive("nwCategorySelect", nwCategorySelectDirective);
 
-  //nwCategorySelectDirective.$inject = ["Category"];
+  nwCategorySelectDirective.$inject = ["Category"];
 
-  function nwCategorySelectDirective() {
+  function nwCategorySelectDirective(Category) {
     return {
+      replace: true,
       restrict: "E",
-      templateUrl: "../templates/directives/nw-category-select.directive.html"
+      templateUrl: "../templates/directives/nw-category-select.directive.html",
+      link: (scope, element, attrs) => {
+        scope.categories = Category.query();
+      },
+      // need refactoring
+      /*controller: ($scope) => {
+        this.activeCategory = () => {
+          return $scope.activeCategory;
+        };
+
+        this.setActiveCategory = (category) => {
+          $scope.activeCategory = category;
+        };
+
+        return this;
+      },*/
+      controller: "nwCategorySelectController",
+      controllerAs: "nwCategorySelectCtrl"
     };
   }
-});
+})();
