118f7d1911cdee7f5803cf718f08543c50a1416b
var-bin <var-bin@users.noreply.github.com>
2017-03-03 12:28:16 +0200

    Refactoring. Use external Controller, arrow function

--- NoteWrangler/app/js/directives/nw-category-select.directive.js
+++ NoteWrangler/app/js/directives/nw-category-select.directive.js
@@ -6,30 +6,16 @@
   angular.module("NoteWrangler")
     .directive("nwCategorySelect", nwCategorySelectDirective);
 
-  nwCategorySelectDirective.$inject = ["Category"];
-
-  function nwCategorySelectDirective(Category) {
+  function nwCategorySelectDirective() {
     return {
       replace: true,
       restrict: "E",
       templateUrl: "../templates/directives/nw-category-select.directive.html",
-      link: function (scope, element, attrs) {
-        scope.categories = Category.query();
+      link: (scope, element, attrs, nwCategorySelectCtrl) => {
+        scope.categories = nwCategorySelectCtrl.query();
       },
-      // need refactoring
-      controller: function ($scope) {
-        this.getActiveCategory = function() {
-          return $scope.activeCategory;
-        };
-
-        this.setActiveCategory = function(newCategory) {
-          $scope.activeCategory = newCategory.name;
-        };
-
-        return this;
-      }
-      /*controller: "nwCategorySelectController",
-      controllerAs: "nwCategorySelectCtrl"*/
+      controller: "nwCategorySelectController",
+      controllerAs: "nwCategorySelectCtrl"
     };
   }
 })();
