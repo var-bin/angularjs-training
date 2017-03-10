f2db9f08af13ade4dcd27237d0d7f60bb6efea37
var-bin <var-bin@users.noreply.github.com>
2017-03-10 16:49:13 +0200

    Replace module

--- A2/quickstart/src/app/modules/app/app.module.ts
+++ A2/quickstart/src/app/modules/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
-import { AppCustomComponent } from './app.custom.component';
+import { AppComponent }  from '../../components/app/app.component';
+import { AppCustomComponent } from '../../components/app-custom/app.custom.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
