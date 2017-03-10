b1daeb03be815454aa14bca4c685770a37f0c23c
var-bin <var-bin@users.noreply.github.com>
2017-03-10 16:48:47 +0200

    Replace components

--- A2/quickstart/src/app/components/app-custom/app.custom.component.ts
+++ A2/quickstart/src/app/components/app-custom/app.custom.component.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
-import { RacePart } from "./race-part.model";
-import { RACE_PARTS } from "./mocks";
+import { RacePart } from "../../models/race-part.model";
+import { RACE_PARTS } from "../../models/mocks";
 
 @Component({
   selector: "my-app-custom-component",
-  templateUrl: "app/app.custom.component.html",
-  styleUrls: [ "app/app.custom.component.css" ]
+  templateUrl: "./app/components/app-custom/app.custom.component.html",
+  styleUrls: [ "./app/components/app-custom/app.custom.component.css" ]
 })
 
 export class AppCustomComponent {
