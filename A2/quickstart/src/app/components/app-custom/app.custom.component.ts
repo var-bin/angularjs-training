fde22ada4105d7ea802df48750256df6e1c7d298
var-bin <var-bin@users.noreply.github.com>
2017-03-13 18:08:24 +0200

    Add methods: upQuantity, downQuantity, cancelRace, enterRace

--- A2/quickstart/src/app/components/app-custom/app.custom.component.ts
+++ A2/quickstart/src/app/components/app-custom/app.custom.component.ts
@@ -31,9 +31,37 @@ export class AppCustomComponent {
     }
 
     return sum;
-  }
+  };
 
   cashLeft() {
     return this.cash - this.totalCost();
-  }
+  };
+
+  upQuantity(race) {
+    if (race.inStock > race.quantity) {
+      return race.quantity++;
+    }
+
+    return false;
+  };
+
+  downQuantity(race) {
+    if (race.quantity > 0) {
+      return race.quantity--;
+    }
+
+    return false;
+  };
+
+  cancelRace(race) {
+    return race.isRacing = false;
+  };
+
+  enterRace(race) {
+    if (this.cashLeft() > race.entryFee) {
+      return race.isRacing = true;
+    }
+
+    alert("You don't have enough cash");
+  };
 };
