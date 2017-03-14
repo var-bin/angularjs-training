85c16344734d1cc086e26f713545aad152f22600
var-bin <var-bin@users.noreply.github.com>
2017-03-14 16:45:43 +0200

    import { FormsModule } from "@angular/forms";. Replace single quotes -> double quotes

--- A2/quickstart/src/app/modules/app/app.module.ts
+++ A2/quickstart/src/app/modules/app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }      from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent }  from '../../components/app/app.component';
-import { AppCustomComponent } from '../../components/app-custom/app.custom.component';
+import { AppComponent }  from "../../components/app/app.component";
+import { AppCustomComponent } from "../../components/app-custom/app.custom.component";
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, AppCustomComponent ],
   bootstrap:    [ AppComponent ]
 })
