1e19be712461e783676ffd4e6152cf74b7f5b43a
var-bin <var-bin@users.noreply.github.com>
2017-03-15 15:29:57 +0200

    Add import { HttpModule } from "@angular/http"; Add HttpModule to imports section in @NgModule

--- A2/quickstart/src/app/modules/app/app.module.ts
+++ A2/quickstart/src/app/modules/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule }      from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
 import { AppComponent }  from "../../components/app/app.component";
 import { AppCustomComponent } from "../../components/app-custom/app.custom.component";
 import { RacingDataService } from "../../services/racing-data.service";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpModule ],
   declarations: [ AppComponent, AppCustomComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ RacingDataService ]
