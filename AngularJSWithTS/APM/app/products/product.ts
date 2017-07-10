714ab55f5f02ecad3c2899499b5482ced47aec8f
var-bin <var-bin@users.noreply.github.com>
2017-07-10 17:57:33 +0300

    Added implementation of class Product. Export interface and class

--- AngularJSWithTS/APM/app/products/product.ts
+++ AngularJSWithTS/APM/app/products/product.ts
@@ -3,7 +3,7 @@
 "use strict";
 
 module app.domain {
-  interface IProduct {
+  export interface IProduct {
     productId: number;
     productName: string;
     productCode: string;
@@ -11,6 +11,21 @@ module app.domain {
     price: number;
     description: string;
     imageUrl: string;
-    calculateDiscount(percent: number): number;
+    /*calculateDiscount(percent: number): number;*/
+  }
+
+  export class Product implements IProduct {
+    constructor(public productId: number,
+                public productName: string,
+                public productCode: string,
+                public releaseDate: Date,
+                public price: number,
+                public description: string,
+                public imageUrl: string) {
+    }
+
+    calculateDiscount(percent: number): number {
+      return this.price - (this.price * percent / 100);
+    }
   }
 }
