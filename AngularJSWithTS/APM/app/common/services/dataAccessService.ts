a926db4cdbd5843f4bb8f9c882743a655813103b
var-bin <var-bin@users.noreply.github.com>
2017-07-17 17:25:25 +0300

    Added implementation for DataAccessService Class. Added  getProductResource() method

--- AngularJSWithTS/APM/app/common/services/dataAccessService.ts
+++ AngularJSWithTS/APM/app/common/services/dataAccessService.ts
@@ -2,11 +2,23 @@
 
 module app.common {
   interface IDataAccessService {
-
+    getProductResource(): ng.resource.IResourceClass<IProductResource>;
   }
 
   interface IProductResource
     extends ng.resource.IResource<app.domain.IProduct> {
 
+  }
+
+  class DataAccessService implements IDataAccessService {
+
+    static $inject = ["$resource"];
+    constructor(private $resource: ng.resource.IResourceService) {
+
+    }
+
+    getProductResource(): ng.resource.IResourceClass<IProductResource> {
+      return this.$resource("/api/prosucts/:productId");
+    }
   }
 }
