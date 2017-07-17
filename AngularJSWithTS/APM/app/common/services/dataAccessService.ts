f799e00103286fb1ba27b224dbbab6e9e26f6d43
var-bin <var-bin@users.noreply.github.com>
2017-07-17 17:04:37 +0300

    Added interfaces

--- AngularJSWithTS/APM/app/common/services/dataAccessService.ts
+++ AngularJSWithTS/APM/app/common/services/dataAccessService.ts
@@ -1,1 +1,12 @@
 // dataAccessService.ts
+
+module app.common {
+  interface IDataAccessService {
+
+  }
+
+  interface IProductResource
+    extends ng.resource.IResource<app.domain.IProduct> {
+
+  }
+}
