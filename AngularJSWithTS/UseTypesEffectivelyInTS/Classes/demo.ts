a55eb520ae8c4b11b66971a7d7bbc18434f0856e
var-bin <var-bin@users.noreply.github.com>
2017-08-03 16:01:18 +0300

    Added Opponent interface and  ComicBookCharacter class

--- AngularJSWithTS/UseTypesEffectivelyInTS/Classes/demo.ts
+++ AngularJSWithTS/UseTypesEffectivelyInTS/Classes/demo.ts
@@ -1,3 +1,23 @@
 // demo.ts
 
 "use strict";
+
+interface Opponent {
+  alias: string;
+  health: number;
+}
+
+class ComicBookCharacter {
+  alias: string;
+  health: number;
+  strength: number;
+  secretIdentity: string;
+
+  attackFunc(opponent: Opponent, attackWith: number) {
+    opponent.health -= attackWith;
+
+    console.log(`${this.alias} attacked ${opponent.alias}, who's health = ${opponent.health}`);
+
+    return opponent.health;
+  }
+}
