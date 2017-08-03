2f28043e91b7f4dce999a9a196cb242e33473f23
var-bin <var-bin@users.noreply.github.com>
2017-08-03 14:03:06 +0300

    Edit value for unit. Add one more example

--- AngularJSWithTS/UseTypesEffectivelyInTS/StringsInTS/demo.ts
+++ AngularJSWithTS/UseTypesEffectivelyInTS/StringsInTS/demo.ts
@@ -3,11 +3,12 @@
 "use strict";
 
 // The stringType variable can be set to any string
-let unit: string;
+let unit: string = "awesome";
 
 // The stringLiteralType can only be set to the type value
 // as well as Null and Undefined, as of now
 let miles: "MILES";
 miles = null; // no error
 miles = undefined; // no error
-miles = "awesome"; // error TS2322: Type '"awesome"' is not assignable to type '"MILES"'.
+// miles = "awesome"; // error TS2322: Type '"awesome"' is not assignable to type '"MILES"'.
+miles = "MILES"; // OK
