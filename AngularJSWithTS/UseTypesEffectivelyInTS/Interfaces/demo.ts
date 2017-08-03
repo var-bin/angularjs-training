55c3f6934d2d31a8790d145619eb716411246dd0
var-bin <var-bin@users.noreply.github.com>
2017-08-03 14:26:40 +0300

    Added examples, comments, interface

--- AngularJSWithTS/UseTypesEffectivelyInTS/Interfaces/demo.ts
+++ AngularJSWithTS/UseTypesEffectivelyInTS/Interfaces/demo.ts
@@ -1,3 +1,24 @@
 // demo.ts
 
 "use strict";
+
+// superHeroName doesn't make sense for a superVillain.
+// The Interface will help us to solve this issue
+
+let superHero: {
+  secretIdentity: string;
+  superHeroName: string;
+  health: number;
+};
+
+let superVillain: {
+  secretIdentity: string;
+  superHeroName: string;
+  health: number;
+};
+
+interface ComicBookCharacter {
+  secretIdentity: string;
+  alias: string;
+  health: number;
+}
