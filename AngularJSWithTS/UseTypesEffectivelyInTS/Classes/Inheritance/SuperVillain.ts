83a0f989195217fe72df2eeded877a283a504d2a
var-bin <var-bin@users.noreply.github.com>
2017-08-03 18:55:52 +0300

    Added SuperVillain own property, method and constructor

--- AngularJSWithTS/UseTypesEffectivelyInTS/Classes/Inheritance/SuperVillain.ts
+++ AngularJSWithTS/UseTypesEffectivelyInTS/Classes/Inheritance/SuperVillain.ts
@@ -2,6 +2,21 @@
 
 import { ComicBookCharacterDefault } from "./ComicBookCharacterDefault";
 
-class SuperVillain extends ComicBookCharacterDefault {}
+class SuperVillain extends ComicBookCharacterDefault {
+  flaws = [
+    "hubris",
+    "always explains evil plan"
+  ];
+
+  getSecretId() {
+    console.log(this.secretIdentity);
+  }
+
+  constructor(a, b, c, d) {
+    super(a, b, c, d);
+
+    console.log(`${this.alias} eats kittens!!!`);
+  }
+}
 
 export { SuperVillain };
