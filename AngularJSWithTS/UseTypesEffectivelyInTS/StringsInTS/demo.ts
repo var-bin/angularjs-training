e89c47f400fdbe302d5b672e4884099907d919ca
var-bin <var-bin@users.noreply.github.com>
2017-08-03 13:59:44 +0300

    Added new example to figure out distinguishing between types of Strings in TypeScript

---
+++ AngularJSWithTS/UseTypesEffectivelyInTS/StringsInTS/demo.ts
@@ -0,0 +1,13 @@
+// demo.ts
+
+"use strict";
+
+// The stringType variable can be set to any string
+let unit: string;
+
+// The stringLiteralType can only be set to the type value
+// as well as Null and Undefined, as of now
+let miles: "MILES";
+miles = null; // no error
+miles = undefined; // no error
+miles = "awesome"; // error TS2322: Type '"awesome"' is not assignable to type '"MILES"'.
