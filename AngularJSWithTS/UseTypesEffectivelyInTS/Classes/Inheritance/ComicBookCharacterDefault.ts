510f6ba0696e873c02e8170fd3c5a13c7993d446
var-bin <var-bin@users.noreply.github.com>
2017-08-03 18:13:10 +0300

    Added new example for Inheritance

---
+++ AngularJSWithTS/UseTypesEffectivelyInTS/Classes/Inheritance/ComicBookCharacterDefault.ts
@@ -0,0 +1,12 @@
+// ComicBookCharacterDefault.ts
+
+"use strict";
+
+class ComicBookCharacterDefault {
+  constructor(
+    public alias: string,
+    public health: number,
+    public strength: number,
+    private secretIdentity: string
+  ) {}
+}
