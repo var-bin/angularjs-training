ad0662977bc91a408d561d2a8f6d4d6b28ca7d1f
var-bin <var-bin@users.noreply.github.com>
2017-08-03 18:43:02 +0300

    Added demo.ts

---
+++ AngularJSWithTS/UseTypesEffectivelyInTS/Classes/Inheritance/demo.ts
@@ -0,0 +1,10 @@
+// demo.ts
+
+import { SuperHero } from "./SuperHero";
+import { SuperVillain } from "./SuperVillain";
+
+let jubilee = new SuperHero("Jubilee", 23, 233, "Jubilation Lee");
+let scarletWitch = new SuperVillain("Scarlet Witch", 233, 4444, "Wanda Maximoff");
+
+console.dir(jubilee);
+console.dir(scarletWitch);
