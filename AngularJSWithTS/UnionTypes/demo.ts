3950cfd3604b82f86d1048fc977c45ecf5cb66d8
var-bin <var-bin@users.noreply.github.com>
2017-08-03 13:30:36 +0300

    Added more examples

--- AngularJSWithTS/UnionTypes/demo.ts
+++ AngularJSWithTS/UnionTypes/demo.ts
@@ -24,3 +24,30 @@ let returnSomeThing = (someThing: thing) => {
 
 returnSomeThing(12345);
 returnSomeThing(["Wonder", "Woman", "Rocket", "Things"]);
+
+// if union type objects with non-objects then
+// the compiler will get mad
+type stuff = string | { name: string };
+
+
+let gimmeStuff = (stuff: stuff) => {
+  typeof stuff === "string";
+  // error
+  typeof stuff.name === "string";
+};
+
+// if union type objects with different types
+// the compiler will get mad
+
+type coolThings = { name: string; } | { id: number; };
+let gimmeCollStuff = (thing: coolThings) => {
+  // error
+  if (typeof thing.name === "string") {
+    return thing.name;
+  }
+
+  // error
+  if (typeof thing.id === "number") {
+    return thing.id;
+  }
+};
