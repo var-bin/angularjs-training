e1581b4a7d85923f2482f386f730817497fe5a60
var-bin <var-bin@users.noreply.github.com>
2017-08-03 18:56:16 +0300

    Added console.log, scarletWitch.getSecretId();

--- AngularJSWithTS/UseTypesEffectivelyInTS/Classes/Inheritance/demo.ts
+++ AngularJSWithTS/UseTypesEffectivelyInTS/Classes/Inheritance/demo.ts
@@ -7,4 +7,7 @@ let jubilee = new SuperHero("Jubilee", 23, 233, "Jubilation Lee");
 let scarletWitch = new SuperVillain("Scarlet Witch", 233, 4444, "Wanda Maximoff");
 
 console.dir(jubilee);
+console.log("/*----------------------------------*/");
 console.dir(scarletWitch);
+
+scarletWitch.getSecretId();
