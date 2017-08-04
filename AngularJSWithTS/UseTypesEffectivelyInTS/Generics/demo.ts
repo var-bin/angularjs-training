546a84db48d09e270dd494a94cd77a06f28b3325
var-bin <var-bin@users.noreply.github.com>
2017-08-04 13:19:52 +0300

    Added SuperHeroes interface. Using Generics

--- AngularJSWithTS/UseTypesEffectivelyInTS/Generics/demo.ts
+++ AngularJSWithTS/UseTypesEffectivelyInTS/Generics/demo.ts
@@ -13,7 +13,7 @@
  * Everything looks OK. Let's see what happens when we make an error.
  * They type argument for type parameter T, cannot be inferred from the usage.
  */
-function pushSomethingIntoCollection<T>(something, collection) {
+function pushSomethingIntoCollection<T>(something: T, collection: T[]) {
   collection.push(something);
 
   console.log(collection);
@@ -31,5 +31,9 @@ let wolverine = {
 let superHeroes = [jeanGrey];
 let powers = ["telekinesis", "esp"];
 
-pushSomethingIntoCollection("cool", superHeroes);
-pushSomethingIntoCollection("adamantium claws", []);
+interface SuperHeroes {
+  name: string;
+}
+
+pushSomethingIntoCollection<SuperHeroes>(wolverine, superHeroes);
+pushSomethingIntoCollection<string>("adamantium claws", powers);
