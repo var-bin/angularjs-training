70ddf5c909821e6140af1645f692cf90159794fb
var-bin <var-bin@users.noreply.github.com>
2017-08-04 11:17:28 +0300

    Added two interfaces: SuperHero; BadGuy. Added let dazzler: SuperHero, let badGuy: BadGuy

--- AngularJSWithTS/UseTypesEffectivelyInTS/Assertion/demo.ts
+++ AngularJSWithTS/UseTypesEffectivelyInTS/Assertion/demo.ts
@@ -1,3 +1,35 @@
 // demo.ts
 
 "use strict";
+
+interface SuperHero {
+  powers: string[];
+  savesTheDay: () => void;
+}
+
+let dazzler: SuperHero = {
+  powers: ["transduces sonic vibrations into light"],
+  savesTheDay() {
+    console.log(`Dazzler ${this.powers} to save the day!!!`);
+  }
+};
+
+interface BadGuy {
+  badDeeds: string[];
+  getRandomBadDeed: () => string;
+  commitBadDeed: () => void;
+}
+
+let badGuy: BadGuy = {
+  badDeeds: [
+    "farts on old folks",
+    "doesn't picks up his dog's poop",
+    "steals from babies"
+  ],
+  getRandomBadDeed() {
+    return this.badDeeds[Math.floor(Math.random() * this.badDeeds.length)];
+  },
+  commitBadDeed() {
+    console.log(`BadGuy ${this.getRandomBadDeed()}`);
+  }
+}
