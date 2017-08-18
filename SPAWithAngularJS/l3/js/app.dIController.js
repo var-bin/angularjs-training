f69f0dc04f315101f8a44194fdd7482f63f96e78
var-bin <var-bin@users.noreply.github.com>
2017-08-18 19:11:18 +0300

    Added DIController

--- SPAWithAngularJS/l3/js/app.dIController.js
+++ SPAWithAngularJS/l3/js/app.dIController.js
@@ -6,7 +6,16 @@
   angular.module("DIApp")
     .controller("DIController", DIController);
 
-  function DIController() {
+  function DIController($filter) {
+    let vm = this;
 
+    vm.name = "Vitalii";
+    vm.upper = upper;
+
+    function upper() {
+      const upCase = $filter("uppercase");
+
+      vm.name = upCase(vm.name);
+    }
   }
 })();
