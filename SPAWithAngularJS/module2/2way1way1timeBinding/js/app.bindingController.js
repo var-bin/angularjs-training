06ec5506480ae3d08ec9a446421fe5e48df71366
var-bin <var-bin@users.noreply.github.com>
2017-08-21 22:54:56 +0300

    Rename file

---
+++ SPAWithAngularJS/module2/2way1way1timeBinding/js/app.bindingController.js
@@ -0,0 +1,12 @@
+// app.bindingController.js
+
+(function() {
+  "use strict";
+
+  angular.module("BindingApp")
+    .controller("BindingController", BindingController);
+
+  function BindingController() {
+    let vm = this;
+  }
+})();
