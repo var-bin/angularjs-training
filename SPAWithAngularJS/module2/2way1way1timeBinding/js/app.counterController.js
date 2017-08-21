9ddf18b5230a52a50e2a060554a275d4d523c680
var-bin <var-bin@users.noreply.github.com>
2017-08-21 22:13:05 +0300

    Init 2way1way1timeBinding

---
+++ SPAWithAngularJS/module2/2way1way1timeBinding/js/app.counterController.js
@@ -0,0 +1,12 @@
+// app.counterController.js
+
+(function() {
+  "use strict";
+
+  angular.module("CounterApp")
+    .controller("CounterController", CounterController);
+
+  function CounterController() {
+    let vm = this;
+  }
+})();
