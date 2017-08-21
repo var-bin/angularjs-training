7ce6c87585e454e8f76e9690c836837481ca4b76
var-bin <var-bin@users.noreply.github.com>
2017-08-21 23:29:56 +0300

    Implemented BindingController

--- SPAWithAngularJS/module2/2way1way1timeBinding/js/app.bindingController.js
+++ SPAWithAngularJS/module2/2way1way1timeBinding/js/app.bindingController.js
@@ -6,7 +6,36 @@
   angular.module("BindingApp")
     .controller("BindingController", BindingController);
 
-  function BindingController() {
+  BindingController.$inject = ["$scope"];
+
+  function BindingController($scope) {
     let vm = this;
+
+    vm.firstName = "Vitalii";
+    //vm.fullName = "";
+    vm.showNumberOfWatchers = showNumberOfWatchers;
+    vm.setFullName = setFullName;
+    vm.logFirstName = logFirstName;
+    vm.logFullName = logFullName;
+
+    function showNumberOfWatchers() {
+      console.log("# of Watchers", $scope.$$watchersCount);
+    }
+
+    function setFullName() {
+      vm.fullName = `${vm.firstName} Rybka`;
+    }
+
+    function logFirstName() {
+      console.log(`First name is: ${vm.firstName}`);
+    }
+
+    function logFullName() {
+      console.log(`Full name is: ${vm.fullName}`);
+    }
+
+    $scope.$watch(() => {
+      console.log("Digest Loop Fired!!!");
+    });
   }
 })();
