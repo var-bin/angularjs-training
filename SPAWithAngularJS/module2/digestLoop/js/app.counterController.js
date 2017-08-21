6454194517a8deab73fd9a9a3c0610f92003bd71
var-bin <var-bin@users.noreply.github.com>
2017-08-21 20:32:32 +0300

    Using , ., .1988watchersCount

--- SPAWithAngularJS/module2/digestLoop/js/app.counterController.js
+++ SPAWithAngularJS/module2/digestLoop/js/app.counterController.js
@@ -6,13 +6,36 @@
   angular.module("CounterApp")
     .controller("CounterController", CounterController);
 
-  function CounterController() {
-    let vm = this;
+  CounterController.$inject = ["$scope"];
 
-    vm.showNumberOfWatchers = showNumberOfWatchers;
+  function CounterController($scope) {
+    $scope.onceCount = 0;
+    $scope.counter = 0;
+
+    $scope.showNumberOfWatchers = showNumberOfWatchers;
+    $scope.countOnce = countOnce;
+    $scope.increment = increment;
 
     function showNumberOfWatchers() {
-
+      console.log("# of Watchers", $scope.$$watchersCount);
     }
+
+    function countOnce() {
+      $scope.onceCount = 1;
+    }
+
+    function increment() {
+      $scope.counter++;
+    }
+
+    $scope.$watch("onceCount", (newValue, oldValue) => {
+      console.log("onceCount newValue: ", newValue);
+      console.log("onceCount oldValue: ", oldValue);
+    });
+
+    $scope.$watch("counter", (newValue, oldValue) => {
+      console.log("counter newValue: ", newValue);
+      console.log("counter oldValue: ", oldValue);
+    });
   }
 })();
