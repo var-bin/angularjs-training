a3ba018d01976cc2e52846d7cfe96948641a2fa8
var-bin <var-bin@users.noreply.github.com>
2017-08-21 22:07:06 +0300

    Added upCounterApply, upCounter methods. Added  service

--- SPAWithAngularJS/module2/digestLoop$digest$apply/js/app.counterController.js
+++ SPAWithAngularJS/module2/digestLoop$digest$apply/js/app.counterController.js
@@ -6,16 +6,43 @@
   angular.module("CounterApp")
     .controller("CounterController", CounterController);
 
-  CounterController.$inject = ["$scope"];
+  CounterController.$inject = ["$scope", "$timeout"];
 
-  function CounterController($scope) {
+  function CounterController($scope, $timeout) {
     let vm = this;
 
     vm.counter = 0;
     vm.upCounter = upCounter;
+    vm.upCounterApply = upCounterApply;
+    vm.upCounter$timeout = upCounter$timeout;
 
     function upCounter() {
-      vm.counter++;
+      setTimeout(function() {
+        vm.counter++;
+
+        console.log("Counter incremented $scope.$digest!");
+
+        $scope.$digest();
+      }, 2000);
+    }
+
+    function upCounterApply() {
+      setTimeout(function() {
+
+        $scope.$apply(function() {
+          vm.counter++;
+
+          console.log("Counter incremented $scope.$apply!");
+        });
+      }, 2000);
+    }
+
+    function upCounter$timeout() {
+      $timeout(function() {
+        vm.counter++;
+
+        console.log("Counter incremented $timeout!");
+      }, 2000);
     }
   }
 })();
