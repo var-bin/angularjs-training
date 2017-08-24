1c6dee7380f93908b038c7d5850577a7f4d4c70e
var-bin <var-bin@users.noreply.github.com>
2017-08-24 21:37:10 +0300

    Investigated Prototypal Inheritance

--- SPAWithAngularJS/module2/prototypalInheritance/js/prototypalInheritance.js
+++ SPAWithAngularJS/module2/prototypalInheritance/js/prototypalInheritance.js
@@ -1,3 +1,33 @@
 // prototypalInheritance.js
 
 "use strict";
+
+let parent = {
+  value: "parentValue",
+  obj: {
+    objValue: "parentObjValue"
+  },
+  walk: () => {
+    console.log("walking");
+  }
+};
+
+let child = Object.create(parent);
+
+console.log("CHILD - child.value: ", child.value);
+console.log("CHILD - child.obj.objValue: ", child.obj.objValue);
+console.log("PARENT - parent.value: ", parent.value);
+console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
+console.log("parent: ", parent);
+console.log("child: ", child);
+
+child.value = "childValue";
+child.obj.objValue = "childObjValue";
+
+console.log("*** CHANGED CHILD - child.value = 'childValue'");
+console.log("*** CHANGED CHILD - child.obj.objValue: 'childObjValue'");
+console.log("CHILD - child.obj.objValue: ", child.obj.objValue);
+console.log("PARENT - parent.value: ", parent.value);
+console.log("PARENT - parent.obj.objValue: ", parent.obj.objValue);
+console.log("parent: ", parent);
+console.log("child: ", child);
