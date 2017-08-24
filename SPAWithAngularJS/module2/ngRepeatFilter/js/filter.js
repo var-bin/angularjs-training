f8154977e394df40c3f4ca66d1a65c63c7a5d0f4
var-bin <var-bin@users.noreply.github.com>
2017-08-24 20:16:29 +0300

    Implemented  filterFive method. Get filteredNumberArray array

--- SPAWithAngularJS/module2/ngRepeatFilter/js/filter.js
+++ SPAWithAngularJS/module2/ngRepeatFilter/js/filter.js
@@ -3,3 +3,11 @@
 let numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log("numberArray: ", numberArray);
+
+function filterFive(value) {
+  return value > 5;
+}
+
+let filteredNumberArray = numberArray.filter(filterFive);
+
+console.log("filteredNumberArray: ", filteredNumberArray);
