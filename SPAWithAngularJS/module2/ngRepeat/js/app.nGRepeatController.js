0c5e57ddcae30ed0e8298e28da06d56a82573912
var-bin <var-bin@users.noreply.github.com>
2017-08-24 20:06:00 +0300

    Implement controller

--- SPAWithAngularJS/module2/ngRepeat/js/app.nGRepeatController.js
+++ SPAWithAngularJS/module2/ngRepeat/js/app.nGRepeatController.js
@@ -3,10 +3,61 @@
 (function() {
   "use strict";
 
-  angular.module("BindingApp")
+  angular.module("MyApp")
     .controller("NgRepeatController", NgRepeatController);
 
-  function NgRepeatController($scope) {
+  function NgRepeatController() {
     let vm = this;
+
+    vm.shoppingList = [
+      "Milk",
+      "Milk",
+      "Donuts",
+      "Cookies",
+      "Chocolate",
+      "Peanut Butter",
+      "Pepto Bismol"
+    ];
+
+    vm.shoppingList1 = [
+      {
+        name: "Milk",
+        quantity: 2
+      },
+      {
+        name: "Donuts",
+        quantity: 200
+      },
+      {
+        name: "Cookies",
+        quantity: 300
+      },
+      {
+        name: "Chocolate",
+        quantity: 5
+      },
+      {
+        name: "Peanut Butter",
+        quantity: 3
+      },
+      {
+        name: "Pepto Bismol",
+        quantity: 7
+      }
+    ];
+
+    vm.addNewItem = addNewItem;
+
+    function addNewItem() {
+      let item = {
+        name: vm.newItemName,
+        quantity: vm.newItemQuantity
+      };
+
+      vm.shoppingList1.push(item);
+
+      vm.newItemName = "";
+      vm.newItemQuantity = "";
+    }
   }
 })();
