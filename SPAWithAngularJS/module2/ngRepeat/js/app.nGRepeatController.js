03e918714a161ae8e3d8d4250af2b3f73cb70715
var-bin <var-bin@users.noreply.github.com>
2017-08-24 19:22:46 +0300

    Init ngRepeat

---
+++ SPAWithAngularJS/module2/ngRepeat/js/app.nGRepeatController.js
@@ -0,0 +1,12 @@
+// app.nGRepeatController.js
+
+(function() {
+  "use strict";
+
+  angular.module("BindingApp")
+    .controller("NgRepeatController", NgRepeatController);
+
+  function NgRepeatController($scope) {
+    let vm = this;
+  }
+})();
