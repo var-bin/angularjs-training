bc87d14cc557b74276a414bab6a4c4485901daa4
var-bin <var-bin@users.noreply.github.com>
2017-08-25 18:15:30 +0300

    Added app.shoppingListShowController.js

---
+++ SPAWithAngularJS/module2/customServices/js/app.shoppingListShowController.js
@@ -0,0 +1,12 @@
+// app.shoppingListShowController.js
+
+(function() {
+  "use strict";
+
+  angular.module("MyApp")
+    .controller("ShoppingListShowController", ShoppingListShowController);
+
+  function ShoppingListShowController() {
+    let vm = this;
+  }
+})();
