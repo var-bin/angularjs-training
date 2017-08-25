a52e104d4b6cded6e670e64ba90c03c5b076ec43
var-bin <var-bin@users.noreply.github.com>
2017-08-25 18:12:52 +0300

    Renamed

---
+++ SPAWithAngularJS/module2/customServices/js/app.shoppingListAddController.js
@@ -0,0 +1,12 @@
+// app.shoppingListAddController.js
+
+(function() {
+  "use strict";
+
+  angular.module("MyApp")
+    .controller("ShoppingListAddController", ShoppingListAddController);
+
+  function ShoppingListAddController() {
+    let vm = this;
+  }
+})();
