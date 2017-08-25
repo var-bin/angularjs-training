6a43a7a1c419a796fd8a187ed06c9580a835db49
var-bin <var-bin@users.noreply.github.com>
2017-08-25 18:05:07 +0300

    Init customServices

---
+++ SPAWithAngularJS/module2/customServices/js/app.myController.js
@@ -0,0 +1,12 @@
+// app.myController.js
+
+(function() {
+  "use strict";
+
+  angular.module("MyApp")
+    .controller("MyController", MyController);
+
+  function MyController() {
+    let vm = this;
+  }
+})();
