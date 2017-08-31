5c54aaef76f861fd4c54aa2ce19bf31e116ae805
var-bin <var-bin@users.noreply.github.com>
2017-08-31 11:00:45 +0300

    Implement ShoppingListController

--- SPAWithAngularJS/module2/providerService/js/app.shoppingListController.js
+++ SPAWithAngularJS/module2/providerService/js/app.shoppingListController.js
@@ -6,7 +6,34 @@
   angular.module("MyApp")
     .controller("ShoppingListController", ShoppingListController);
 
-  function ShoppingListController() {
+  ShoppingListController.$inject = ["ShoppingListService"];
+
+  function ShoppingListController(ShoppingListService) {
     let list = this;
+
+    list.items = ShoppingListService.getItems();
+
+
+    list.addItem = addItem;
+    list.removeItem = removeItem;
+
+    function addItem() {
+      try {
+        ShoppingListService.addItem(list.itemName, list.itemQuantity);
+
+        list.itemName = "";
+        list.itemQuantity = "";
+      } catch (error) {
+        list.errorMessage = error;
+      }
+    }
+
+    function removeItem(index) {
+      if (index < ShoppingListService.getMaxItems()) {
+        list.errorMessage = "";
+      }
+
+      ShoppingListService.removeItem(index);
+    }
   }
 })();
