ee7c887b694d741cb3584ca859970484a479e37b
var-bin <var-bin@users.noreply.github.com>
2017-08-31 15:55:24 +0300

    Init app.alreadyBoughtController.js

---
+++ SPAWithAngularJS/module2/module2-solution/js/app.alreadyBoughtController.js
@@ -0,0 +1,12 @@
+// app.alreadyBoughtController.js
+
+(function() {
+  "use strict";
+
+  angular.module("ShoppingListCheckOff")
+    .controller("AlreadyBoughtController", AlreadyBoughtController);
+
+  function AlreadyBoughtController() {
+    let alreadyBoughtCtrl = this;
+  }
+})();
