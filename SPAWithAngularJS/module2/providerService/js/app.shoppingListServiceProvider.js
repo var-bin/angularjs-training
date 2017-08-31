3851195d95578649ea8de96a0f993dfc519a2b0e
var-bin <var-bin@users.noreply.github.com>
2017-08-31 10:53:44 +0300

    Added  ShoppingListServiceProvider, ShoppingListService implementations

--- SPAWithAngularJS/module2/providerService/js/app.shoppingListServiceProvider.js
+++ SPAWithAngularJS/module2/providerService/js/app.shoppingListServiceProvider.js
@@ -4,7 +4,57 @@
   "use strict";
 
   angular.module("MyApp")
-    .provider("ShoppingListService", ShoppingListService);
+    .provider("ShoppingListService", ShoppingListServiceProvider);
 
-  function ShoppingListService() {}
+  // If not specified, maxItems assumed unlimited
+  function ShoppingListService(maxItems) {
+    let service = this;
+
+    // List of Shopping items
+    let items = [];
+
+    service.addItem = addItem;
+    service.getItems = getItems;
+    service.removeItem = removeItem;
+
+    function addItem(itemName, itemQuantity) {
+      // !maxItems -> maxItems === undefuned
+      // maxItems -> maxItems !== undefuned
+
+
+      if (!maxItems ||
+          maxItems && items.length < maxItems) {
+        let item = {
+          name: itemName,
+          quantity: itemQuantity
+        };
+
+        items.push(item);
+      } else {
+        throw new Error(`Max items (${maxItems}) reached.`);
+      }
+    }
+
+    function getItems() {
+      return items;
+    }
+
+    function removeItem(itemIndex) {
+      items.splice(itemIndex, 1);
+    }
+  }
+
+  function ShoppingListServiceProvider() {
+    let provider = this;
+
+    provider.default = {
+      maxItems: 10
+    };
+
+    provider.$get = () => {
+      const shoppingList = new ShoppingListService(provider.default.maxItems);
+
+      return shoppingList;
+    };
+  }
 })();
