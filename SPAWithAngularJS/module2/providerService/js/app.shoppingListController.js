1b9f40abfa7d5498b761e5ae7e4fd8c407444e77
var-bin <var-bin@users.noreply.github.com>
2017-08-31 10:22:25 +0300

    Init providerService

---
+++ SPAWithAngularJS/module2/providerService/js/app.shoppingListController.js
@@ -0,0 +1,12 @@
+// app.shoppingListController.js
+
+(function() {
+  "use strict";
+
+  angular.module("MyApp")
+    .controller("ShoppingListController", ShoppingListController);
+
+  function ShoppingListController() {
+    let list = this;
+  }
+})();
