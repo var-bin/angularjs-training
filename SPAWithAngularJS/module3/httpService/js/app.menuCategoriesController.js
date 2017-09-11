63693b1f54b8772e7ea1b1f12f6f70b43e1be638
var-bin <var-bin@users.noreply.github.com>
2017-09-11 13:57:06 +0300

    Init httpService

---
+++ SPAWithAngularJS/module3/httpService/js/app.menuCategoriesController.js
@@ -0,0 +1,12 @@
+// app.menuCategoriesController.js
+
+(function() {
+  "use strict";
+
+  angular.module("MenuCategoriesApp")
+    .controller("MenuCategoriesController", MenuCategoriesController);
+
+  function MenuCategoriesController() {
+    let vm = this;
+  }
+})();
