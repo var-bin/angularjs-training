e5cd844f3b568d7682e449c07099de7b5c630e1f
var-bin <var-bin@users.noreply.github.com>
2017-09-21 19:08:11 +0300

    Edit cookiesInList method

--- SPAWithAngularJS/module3/directives/directivesAPIs/js/app.shoppingListDirectiveController.js
+++ SPAWithAngularJS/module3/directives/directivesAPIs/js/app.shoppingListDirectiveController.js
@@ -13,10 +13,11 @@
 
     function cookiesInList() {
       const TRIGGERED_WORD = "cookie";
+      //console.log(this, list.items);
 
-      if (list.list.items.length) {
-        for (let i = 0; i < list.list.items.length; i++) {
-          const name = list.list.items[i].name;
+      if (list.items.length) {
+        for (let i = 0; i < list.items.length; i++) {
+          const name = list.items[i].name;
 
           if (name.toLowerCase().indexOf(TRIGGERED_WORD) !== -1) {
             return true;
