c0709aeb5a385da155d4e021d894e8c29c56731a
var-bin <var-bin@users.noreply.github.com>
2017-10-10 19:10:11 +0300

    Implemented cookiesInList, removeItem methods

--- SPAWithAngularJS/module4/components/js/app.shoppingListComponent.controller.js
+++ SPAWithAngularJS/module4/components/js/app.shoppingListComponent.controller.js
@@ -10,9 +10,26 @@
     let $ctrl = this;
 
     $ctrl.cookiesInList = cookiesInList;
+    $ctrl.removeItem = removeItem;
 
     function cookiesInList() {
-      console.log($ctrl.items.length);
+      const TRIGGERED_WORD = "cookie";
+
+      for (let item of $ctrl.items) {
+        if (item.name.toLowerCase().indexOf(TRIGGERED_WORD) !== -1) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    function removeItem(itemIndex) {
+      // comes from onRemove bindings property
+      // and take method from the parenrt controller
+      $ctrl.onRemove({
+        index: itemIndex
+      })
     }
   }
 })();
