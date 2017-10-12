b85bd6f2a82c103969546e772f8db33b2bd710fc
var-bin <var-bin@users.noreply.github.com>
2017-10-12 17:58:29 +0300

    Edit test description

--- jest-testing/obj/obj.test.ts
+++ jest-testing/obj/obj.test.ts
@@ -4,6 +4,6 @@
 
 import { data } from "./obj";
 
-test("The data object should contain {one: 1, two: 2}", () => {
+test("The data object should contain at least {one: 1, two: 2}", () => {
   expect(data).toMatchSnapshot();
 });
