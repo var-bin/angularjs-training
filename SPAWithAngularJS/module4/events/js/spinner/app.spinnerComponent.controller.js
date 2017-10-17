6f5b450a4f0a798d73e4fcdaeace56d72b275a56
var-bin <var-bin@users.noreply.github.com>
2017-10-17 15:59:06 +0300

    Implemented SpinnerComponentController functionality

--- SPAWithAngularJS/module4/events/js/spinner/app.spinnerComponent.controller.js
+++ SPAWithAngularJS/module4/events/js/spinner/app.spinnerComponent.controller.js
@@ -6,8 +6,33 @@
   angular.module("ShoppingListEventsApp")
     .controller("SpinnerComponentController", SpinnerComponentController);
 
-  function SpinnerComponentController() {
-    let $ctrl = this;
+  SpinnerComponentController.$inject = ["$rootScope"];
 
+  function SpinnerComponentController($rootScope) {
+    let $ctrl = this;
+    const onOff = false;
+
+    $ctrl.$doCheck = $doCheck;
+
+    function showSpinner() {
+      const spinnerHolder = document.querySelector(".l-spinner");
+
+      $rootScope.$on("shoppinglist:processing", (event, data) => {
+        console.log("Event: ", event);
+        console.log("Data: ", data);
+
+        if (data.on) {
+          spinnerHolder.classList.add("is-active");
+
+          return;
+        }
+
+        spinnerHolder.classList.remove("is-active");
+      });
+    }
+
+    function $doCheck() {
+      showSpinner();
+    }
   }
 })();
