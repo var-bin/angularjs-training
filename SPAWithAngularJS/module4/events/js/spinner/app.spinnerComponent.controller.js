850d218d6418615d43a9c4068c2791c749178470
var-bin <var-bin@users.noreply.github.com>
2017-10-17 14:41:44 +0300

    Added spinner component controller

---
+++ SPAWithAngularJS/module4/events/js/spinner/app.spinnerComponent.controller.js
@@ -0,0 +1,13 @@
+// app.spinnerComponent.controller.js
+
+(function() {
+  "use strict";
+
+  angular.module("ShoppingListEventsApp")
+    .controller("SpinnerComponentController", SpinnerComponentController);
+
+  function SpinnerComponentController() {
+    let $ctrl = this;
+
+  }
+})();
