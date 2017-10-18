28088dd34455a527464bdbbec0b9c826bc5796af
var-bin <var-bin@users.noreply.github.com>
2017-10-18 11:55:52 +0300

    First version

--- SPAWithAngularJS/module5/jasmine/src/cookieDetector.js
+++ SPAWithAngularJS/module5/jasmine/src/cookieDetector.js
@@ -3,7 +3,7 @@
 "use strict";
 
 // v1
-/* function detectCookie(items) {
+function detectCookie(items) {
   const TRIGGERED_WORD = "cookie";
 
   for (let i = 0; i < items.length; i++) {
@@ -15,20 +15,6 @@
 
     return false;
   }
-} */
-
-function detectCookie(items) {
-  const TRIGGERED_WORD = "cookie";
-
-  for (let i = 0; i < items.length; i++) {
-    const item = items[i];
-
-    if (item.indexOf(TRIGGERED_WORD) !== -1) {
-      return true;
-    }
-  }
-
-  return false;
 }
 
 module.exports = detectCookie;
