03d6e344e3a5c8e31cf9ca0cba69a8234eebe680
var-bin <var-bin@users.noreply.github.com>
2017-10-18 13:12:57 +0300

    Added jasmine spec for oddEven.js

---
+++ SPAWithAngularJS/module5/jasmine/spec/oddEven.spec.js
@@ -0,0 +1,31 @@
+// oddEven.spec.js
+
+"use strict";
+
+describe("oddEven", () => {
+  let randomNumGenerator8;
+  let randomNumGenerator3;
+  const getRandomOddEven1to10 = require("../src/oddEven");
+
+  beforeEach(() => {
+    randomNumGenerator8 = (from, to) => {
+      return 8;
+    };
+
+    randomNumGenerator3 = (from, to) => {
+      return 3;
+    };
+  });
+
+  it("Should produce an odd number", () => {
+    const result = getRandomOddEven1to10("odd", randomNumGenerator3);
+
+    expect(result).toEqual(3);
+  });
+
+  it("Should produce an even number", () => {
+    const result = getRandomOddEven1to10("even", randomNumGenerator8);
+
+    expect(result).toEqual(8);
+  });
+});
