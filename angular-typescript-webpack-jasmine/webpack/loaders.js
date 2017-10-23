94219e4c2d5bcf75bf02409f551f1946237408e1
var-bin <var-bin@users.noreply.github.com>
2017-10-23 14:50:26 +0300

    Refactoring: Replace single quotes -> double quotes

--- angular-typescript-webpack-jasmine/webpack/loaders.js
+++ angular-typescript-webpack-jasmine/webpack/loaders.js
@@ -1,30 +1,30 @@
 module.exports = [
     {
         test: /\.ts(x?)$/,
-        loader: 'ts-loader'
+        loader: "ts-loader"
     }, {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        loader: "style-loader!css-loader"
     }, {
         test: /\.scss$/,
-        loader: 'style-loader!css-loader!sass-loader'
+        loader: "style-loader!css-loader!sass-loader"
     }, {
         test: /\.html$/,
         exclude: /node_modules/,
-        loader: 'raw-loader'
+        loader: "raw-loader"
     }, {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
     }, {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
+        loader: "file-loader"
     }, {
         test: /\.jpg$/,
         exclude: /node_modules/,
-        loader: 'file-loader'
+        loader: "file-loader"
     }, {
         test: /\.png$/,
         exclude: /node_modules/,
-        loader: 'url-loader'
+        loader: "url-loader"
     }
 ];
