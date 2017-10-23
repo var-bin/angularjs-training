66eb69e11c28116d33245a7166d14643ae79159a
var-bin <var-bin@users.noreply.github.com>
2017-10-23 13:47:37 +0300

    Fix typos

--- angular-typescript-webpack-jasmine/webpack/loaders.js
+++ angular-typescript-webpack-jasmine/webpack/loaders.js
@@ -7,11 +7,11 @@ module.exports = [
         loader: 'style-loader!css-loader'
     }, {
         test: /\.scss$/,
-        loader: 'style!css!sass'
+        loader: 'style-loader!css-loader!sass-loader'
     }, {
         test: /\.html$/,
         exclude: /node_modules/,
-        loader: 'raw'
+        loader: 'raw-loader'
     }, {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'url-loader?limit=10000&mimetype=application/font-woff'
@@ -19,12 +19,12 @@ module.exports = [
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'file-loader'
     }, {
-        test: '\.jpg$',
+        test: /\.jpg$/,
         exclude: /node_modules/,
-        loader: 'file'
+        loader: 'file-loader'
     }, {
-        test: '\.png$',
+        test: /\.png$/,
         exclude: /node_modules/,
-        loader: 'url'
+        loader: 'url-loader'
     }
 ];
