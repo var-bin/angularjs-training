cc7416d8713f67d4e31843ce4e00b4889ee4ec0c
var-bin <var-bin@users.noreply.github.com>
2017-10-23 15:05:51 +0300

    Delete useless trailing white spaces

--- angular-typescript-webpack-jasmine/src/modules/application/angular/components/twitterApplication/TwitterApplicationComponent.ts
+++ angular-typescript-webpack-jasmine/src/modules/application/angular/components/twitterApplication/TwitterApplicationComponent.ts
@@ -3,11 +3,11 @@ export class TwitterApplicationComponent implements ng.IComponentOptions {
         <nav class="navbar navbar-inverse navbar-fixed-top">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <button type="button" 
-                    class="navbar-toggle collapsed" 
-                    data-toggle="collapse" 
-                    data-target="#navbar" 
-                    aria-expanded="false" 
+                    <button type="button"
+                    class="navbar-toggle collapsed"
+                    data-toggle="collapse"
+                    data-target="#navbar"
+                    aria-expanded="false"
                     aria-controls="navbar">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
