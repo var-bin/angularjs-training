e218a06bd1d88c8b7eeb795aa112cc949986f54b
var-bin <var-bin@users.noreply.github.com>
2017-10-24 13:56:43 +0300

    Refactoring rules

--- angular-typescript-webpack-jasmine/webpack/rules.js
+++ angular-typescript-webpack-jasmine/webpack/rules.js
@@ -1,37 +1,80 @@
-module.exports = [
+const rules = [
+    // TypeScript
     {
         test: /\.ts(x?)$/,
         loader: "ts-loader"
-    }, {
+    },
+
+    // TSLint
+    {
         test: /\.ts$/,
         loader: "tslint-loader",
         options: {
             emitErrors: true,
             failOnHint: true
         }
-    }, {
+    },
+
+    // CSS
+    {
         test: /\.css$/,
-        loader: "style-loader!css-loader"
-    }, {
+        use: [{
+            loader: "style-loader"
+        }, {
+            loader: "css-loader"
+        }]
+    },
+
+    // SCSS
+    {
         test: /\.scss$/,
-        loader: "style-loader!css-loader!sass-loader"
-    }, {
+        use: [{
+            loader: "style-loader"
+        }, {
+            loader: "css-loader"
+        }, {
+            loader: "sass-loader"
+        }]
+    },
+
+    // HTML
+    {
         test: /\.html$/,
         exclude: /node_modules/,
         loader: "raw-loader"
-    }, {
+    },
+
+    // Fonts (woff, woff2)
+    {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff"
-    }, {
+        use: [{
+            loader: "url-loader",
+            options: {
+                limit: 10000,
+                mimetype: "application/font-woff"
+            }
+        }]
+    },
+
+    // Fonts (ttf, eot, svg)
+    {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: "file-loader"
-    }, {
+    },
+
+    // jpg
+    {
         test: /\.jpg$/,
         exclude: /node_modules/,
         loader: "file-loader"
-    }, {
+    },
+
+    // png
+    {
         test: /\.png$/,
         exclude: /node_modules/,
         loader: "url-loader"
     }
 ];
+
+module.exports = rules;
