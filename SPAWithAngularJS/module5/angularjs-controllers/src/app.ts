d114c357c9fee08a7a6b93f9e66ec0bf1051de00
var-bin <var-bin@users.noreply.github.com>
2017-10-28 23:48:31 +0300

    Added import "../css/styles.css";

--- SPAWithAngularJS/module5/angularjs-controllers/src/app.ts
+++ SPAWithAngularJS/module5/angularjs-controllers/src/app.ts
@@ -9,6 +9,7 @@ import "./message/app.message.controller";
 import "./message1/app.message1.controller";
 import "./sum/sum";
 
+import "../css/styles.css";
 
 bootstrap(document, ["myApp"], {
   strictDi: true
