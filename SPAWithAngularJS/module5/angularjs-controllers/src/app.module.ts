0804971421d13e679e261d64c7cc19964d9047f2
var-bin <var-bin@users.noreply.github.com>
2017-11-01 15:10:27 +0200

    Added config for routing

--- SPAWithAngularJS/module5/angularjs-controllers/src/app.module.ts
+++ SPAWithAngularJS/module5/angularjs-controllers/src/app.module.ts
@@ -3,7 +3,31 @@
 "use strict";
 
 import { module } from "angular";
+import uiRoute from "@uirouter/angularjs";
 
-const myApp = module("myApp", []);
+const myApp = module("myApp", [uiRoute])
+  .config(routingConfig);
+
+routingConfig.$inject = ["$stateProvider", "$urlRouterProvider"];
+
+function routingConfig(
+  $stateProvider: angular.ui.IStateProvider,
+  $urlRouterProvider: angular.ui.IUrlRouterProvider) {
+  const message1State: angular.ui.IState = {
+    name: "message1",
+    url: "/message1",
+    template: "<message-1-component></message-1-component>"
+  };
+
+  const home: angular.ui.IState = {
+    name: "home",
+    url: "/"
+  }
+
+  $stateProvider.state(message1State);
+  $stateProvider.state(home);
+
+  $urlRouterProvider.otherwise("home");
+}
 
 export { myApp };
