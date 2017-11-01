6cad2954da593a3631033431452b7a86018a8463
var-bin <var-bin@users.noreply.github.com>
2017-11-01 17:24:54 +0200

    Using . Added comments. Replace template: "<message-1-component></message-1-component>" -> component: "message1Component"

--- SPAWithAngularJS/module5/angularjs-controllers/src/app.module.ts
+++ SPAWithAngularJS/module5/angularjs-controllers/src/app.module.ts
@@ -4,19 +4,24 @@
 
 import { module } from "angular";
 
-RoutesConfig.$inject = ["$stateProvider"];
+RoutesConfig.$inject = ["$stateProvider", "$urlRouterProvider"];
 
-function RoutesConfig($stateProvider: angular.ui.IStateProvider) {
+function RoutesConfig(
+  $stateProvider: angular.ui.IStateProvider,
+  $urlRouterProvider: angular.ui.IUrlRouterProvider) {
   const message1State: angular.ui.IState = {
     name: "message1",
     url: "/message1",
-    template: "<message-1-component></message-1-component>"
+    component: "message1Component"
   };
 
   const home: angular.ui.IState = {
     name: "home",
     url: "/"
   };
+
+  // Redirect to home if no other URL matches
+  $urlRouterProvider.otherwise("/");
 
   $stateProvider.state(message1State);
   $stateProvider.state(home);
