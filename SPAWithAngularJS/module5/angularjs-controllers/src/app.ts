ca0915e11f60580cfdfcc04074aa42ed4aa72a0b
var-bin <var-bin@users.noreply.github.com>
2017-11-13 10:46:14 +0200

    Refactoring

--- SPAWithAngularJS/module5/angularjs-controllers/src/app.ts
+++ SPAWithAngularJS/module5/angularjs-controllers/src/app.ts
@@ -3,18 +3,10 @@
 "use strict";
 
 import { bootstrap } from "angular";
-import { myAppModule } from "./app.module";
-import { Message1Component } from "./message1/app.message1.controller";
+import "./app.module";
 import "./sum/sum";
-import { RepositoriesComponent } from "./repositories/app.repositories.component";
-import { RepositoriesService } from "./repositories/app.repositories.service";
 
 import "../css/styles.css";
-
-myAppModule
-  .component("message1Component", new Message1Component())
-  .component("repositoriesComponent", new RepositoriesComponent())
-  .service("RepositoriesService", RepositoriesService);
 
 bootstrap(document, ["myApp"], {
   strictDi: true
