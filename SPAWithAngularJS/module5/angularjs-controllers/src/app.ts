4a15781ffde82d5cc960513c52ef5af99cad75d4
var-bin <var-bin@users.noreply.github.com>
2017-11-13 10:05:40 +0200

    Added new .component() and .service()

--- SPAWithAngularJS/module5/angularjs-controllers/src/app.ts
+++ SPAWithAngularJS/module5/angularjs-controllers/src/app.ts
@@ -6,11 +6,15 @@ import { bootstrap } from "angular";
 import { myAppModule } from "./app.module";
 import { Message1Component } from "./message1/app.message1.controller";
 import "./sum/sum";
+import { RepositoriesComponent } from "./repositories/app.repositories.component";
+import { RepositoriesService } from "./repositories/app.repositories.service";
 
 import "../css/styles.css";
 
 myAppModule
-  .component("message1Component", new Message1Component());
+  .component("message1Component", new Message1Component())
+  .component("repositoriesComponent", new RepositoriesComponent())
+  .service("RepositoriesService", RepositoriesService);
 
 bootstrap(document, ["myApp"], {
   strictDi: true
