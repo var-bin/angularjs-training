96084858d3a48f1424915816ed7a206077fc937a
var-bin <var-bin@users.noreply.github.com>
2017-11-13 18:01:32 +0200

    Delete useless code

--- SPAWithAngularJS/module5/angularjs-controllers/src/message1/app.message1.controller.ts
+++ SPAWithAngularJS/module5/angularjs-controllers/src/message1/app.message1.controller.ts
@@ -11,32 +11,11 @@ class Message1Ctrl {
 }
 
 class Message1Component implements ng.IComponentOptions {
-  public template: any;
+  public template: string = require("./app.message1.view.html");
   public controller: any = Message1Ctrl;
   public bindings: {
     myData: "<"
-  }
-
-  constructor() {
-    this.getTemplate()
-      .then((_t) => {
-        this.template = _t;
-      });
-  }
-
-  getTemplate() {
-    return new Promise((resolve, reject) => {
-      require.ensure(["./app.message1.view.html"], () => {
-        const _t = require("./app.message1.view.html");
-
-        if (_t) {
-          resolve(_t)
-        } else {
-          reject(new Error("Network Error"));
-        }
-      });
-    });
-  }
+  };
 }
 
 export { Message1Component };
