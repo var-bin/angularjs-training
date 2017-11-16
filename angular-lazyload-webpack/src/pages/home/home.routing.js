f277678fadb2fe2a0492e6c1497b803a0e45cc56
var-bin <var-bin@users.noreply.github.com>
2017-11-16 17:00:43 +0200

    Refactoring. Replace single quotes -> double quotes.

--- angular-lazyload-webpack/src/pages/home/home.routing.js
+++ angular-lazyload-webpack/src/pages/home/home.routing.js
@@ -1,33 +1,33 @@
-'use strict';
+"use strict";
 
 function homeRouting($urlRouterProvider, $stateProvider) {
-  $urlRouterProvider.otherwise('/home');
+  $urlRouterProvider.otherwise("/home");
 
   $stateProvider
-    .state('home', {
-      url: '/home',
-      template: require('./views/home.html'), // include small templates into routing configuration
-      controller: 'HomeController as vm',
+    .state("home", {
+      url: "/home",
+      template: require("./views/home.html"), // include small templates into routing configuration
+      controller: "HomeController as vm",
       resolve: {
         loadHomeController: ($q, $ocLazyLoad) => {
           return $q((resolve) => {
             require.ensure([], () => {
               // load whole module
-              let module = require('./home');
-              $ocLazyLoad.load({name: 'home'});
+              let module = require("./home");
+              $ocLazyLoad.load({name: "home"});
 
               resolve(module.controller);
             });
           });
         }
       }
-    }).state('home.about', {
-      url: '/about',
-      template: require('./views/home.about.html'),
-      controller: 'HomeAboutController as vm',
+    }).state("home.about", {
+      url: "/about",
+      template: require("./views/home.about.html"),
+      controller: "HomeAboutController as vm",
     });
 }
 
 export default angular
-  .module('home.routing', [])
+  .module("home.routing", [])
   .config(homeRouting);
