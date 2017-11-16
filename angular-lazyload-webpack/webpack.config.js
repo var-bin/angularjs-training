1210e8cef1df4513bcc3e955a6d15f93f4fc8bea
var-bin <var-bin@users.noreply.github.com>
2017-11-16 17:32:40 +0200

    Delete useless configs. Added HtmlWebpackPlugin,  webpack.optimize.CommonsChunkPlugin

--- angular-lazyload-webpack/webpack.config.js
+++ angular-lazyload-webpack/webpack.config.js
@@ -2,6 +2,7 @@
 
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
   entry: {
@@ -15,18 +16,31 @@ const config = {
     root: path.resolve(__dirname + "/src/")
   },
   module: {
-    noParse: [
-      "angular"
-    ],
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
       loader: "ng-annotate!babel"
     }, {
       test: /\.html$/,
-      loader: "raw"
+      loader: "raw",
+      exclude: "index.html"
     }]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html"
+    }),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "common",
+      filename: "common.js",
+      minChunks: (module) => {
+        // this assumes your vendor imports exist in the node_modules directory
+        return module.context && module.context.indexOf("node_modules") !== -1;
+      }
+    })
+  ]
 };
 
 module.exports = config;
