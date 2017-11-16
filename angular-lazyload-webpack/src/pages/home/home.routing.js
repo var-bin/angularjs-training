556b47175a7f7449ab3cb6cad74d2f6f4a061d10
var-bin <var-bin@users.noreply.github.com>
2017-11-16 22:21:35 +0200

    Delete useless state

--- angular-lazyload-webpack/src/pages/home/home.routing.js
+++ angular-lazyload-webpack/src/pages/home/home.routing.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const angular = require("angular");
+
 function homeRouting($urlRouterProvider, $stateProvider) {
   $urlRouterProvider.otherwise("/home");
 
@@ -7,24 +9,26 @@ function homeRouting($urlRouterProvider, $stateProvider) {
     .state("home", {
       url: "/home",
       template: require("./views/home.html"), // include small templates into routing configuration
-      controller: "HomeController as vm",
       resolve: {
-        loadHomeController: ($q, $ocLazyLoad) => {
-          return $q((resolve) => {
+        loadHomeModule: ($ocLazyLoad) => {
+          return new Promise((resolve, reject) => {
             require.ensure([], () => {
               // load whole module
               let module = require("./home");
-              $ocLazyLoad.load({name: "home"});
 
-              resolve(module.controller);
+              $ocLazyLoad.load({
+                name: "home"
+              });
+
+              if (module.name) {
+                resolve(module.name);
+              } else {
+                reject("Ooops, somethig went wrong!");
+              }
             });
           });
         }
       }
-    }).state("home.about", {
-      url: "/about",
-      template: require("./views/home.about.html"),
-      controller: "HomeAboutController as vm",
     });
 }
 
