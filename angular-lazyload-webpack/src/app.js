e99b21f23867bda2e50e18dc0ed75db39d727cf0
var-bin <var-bin@users.noreply.github.com>
2017-11-16 16:51:38 +0200

    Refactoring. Replace require

--- angular-lazyload-webpack/src/app.js
+++ angular-lazyload-webpack/src/app.js
@@ -1,15 +1,14 @@
 'use strict';
 
-export default require('angular')
+const angular = require('angular');
+require('angular-ui-router');
+require('oclazyload');
+
+const HOME_ROUTING = require('./pages/home/home.routing');
+
+export default angular
   .module('lazyApp', [
-    require('angular-ui-router'),
-    // @TODO: It's a hack! https://github.com/ocombe/ocLazyLoad/issues/179
-    (() => { require('oclazyload'); return 'oc.lazyLoad' })(),
-    /*
-      uncomment to move msg-store to bundle.js only instead
-      of putting it in both: 3.bundle.js and 4.bundle.js
-     */
-    //require('commons/msg-store').name,
-    require('./pages/home/home.routing').name,
-    require('./pages/messages/messages.routing').name,
+    'ui.router',
+    'oc.lazyLoad',
+    HOME_ROUTING.name
   ]);
