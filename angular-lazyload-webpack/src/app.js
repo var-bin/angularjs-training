527f2503b93f3668498d21403d89a98a818ae4e0
var-bin <var-bin@users.noreply.github.com>
2017-11-16 16:53:27 +0200

    Refactoring. Replace single quotes -> double quotes

--- angular-lazyload-webpack/src/app.js
+++ angular-lazyload-webpack/src/app.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-const angular = require('angular');
-require('angular-ui-router');
-require('oclazyload');
+const angular = require("angular");
+require("angular-ui-router");
+require("oclazyload");
 
-const HOME_ROUTING = require('./pages/home/home.routing');
+const HOME_ROUTING = require("./pages/home/home.routing");
 
 export default angular
-  .module('lazyApp', [
-    'ui.router',
-    'oc.lazyLoad',
+  .module("lazyApp", [
+    "ui.router",
+    "oc.lazyLoad",
     HOME_ROUTING.name
   ]);
