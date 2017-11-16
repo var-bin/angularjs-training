4c9deb333b46e131d8c937878cef5ddec65ebb88
var-bin <var-bin@users.noreply.github.com>
2017-11-16 17:01:55 +0200

    Refactoring. Replace single quotes -> double quotes.

--- angular-lazyload-webpack/src/pages/home/controllers/home.controller.js
+++ angular-lazyload-webpack/src/pages/home/controllers/home.controller.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 class HomeController {
   constructor() {}
 }
 
 const HOME_CTRL = angular
-  .module('home.controller', [])
-  .controller('HomeController', HomeController);
+  .module("home.controller", [])
+  .controller("HomeController", HomeController);
 
 export { HOME_CTRL };
