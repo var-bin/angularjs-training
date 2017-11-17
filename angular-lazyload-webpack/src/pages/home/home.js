06b76891a96be34a3e1ff0b92ba94f6aae07baf0
var-bin <var-bin@users.noreply.github.com>
2017-11-17 22:04:40 +0200

    Rename HOME_CTRL -> HOME_MODULE

--- angular-lazyload-webpack/src/pages/home/home.js
+++ angular-lazyload-webpack/src/pages/home/home.js
@@ -1,10 +1,10 @@
 "use strict";
 
-import { HOME_CTRL } from "./controllers/home.controller";
+import { HOME_MODULE } from "./controllers/home.controller";
 import { HOME_ABOUT_CTRL } from "./controllers/home.about.controller";
 
 export default angular
   .module("home", [
-    HOME_CTRL,
+    HOME_MODULE,
     HOME_ABOUT_CTRL
   ]);
