410b90e1dc33d62f49479d45f5a5a5ee808cfc69
var-bin <var-bin@users.noreply.github.com>
2017-11-20 12:05:22 +0200

    Delete useless code

--- angular-lazyload-webpack/src/pages/home/home.module.routing.js
+++ angular-lazyload-webpack/src/pages/home/home.module.routing.js
@@ -8,7 +8,6 @@ function homeRouting($urlRouterProvider, $stateProvider) {
   $stateProvider
     .state("home", {
       url: "/home",
-      //template: require("./views/home.html"), // include small templates into routing configuration
       component: "homeComponent",
       lazyLoad: ($transition$) => {
         const $ocLazyLoad = $transition$.injector().get("$ocLazyLoad");
