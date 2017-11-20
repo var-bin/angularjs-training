e46c52472ab79371959d6f8cc2a871cdabb32632
var-bin <var-bin@users.noreply.github.com>
2017-11-20 15:04:12 +0200

    Delete useless files

--- angular-lazyload-webpack/src/pages/messages/controllers/messages.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-class MessagesController {
-  constructor() {}
-}
-
-export default angular
-  .module('messages.controller', [])
-  .controller('MessagesController', MessagesController);
