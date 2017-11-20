9976f0ca67414465535f43064c29a171ea45d4ec
var-bin <var-bin@users.noreply.github.com>
2017-11-20 11:19:52 +0200

    Replace files and rename for home index module

---
+++ angular-lazyload-webpack/src/pages/home/index/index.module.js
@@ -0,0 +1,20 @@
+"use strict";
+
+class HomeIndexController {
+  constructor() {
+    this.title = "HomeComponent";
+  }
+}
+
+class HomeIndexComponent {
+  constructor() {
+    this.template = require("./index.view.html");
+    this.controller = HomeIndexController;
+  }
+}
+
+const HOME_INDEX_MODULE = angular
+  .module("home.module", [])
+  .component("homeComponent", new HomeIndexComponent());
+
+export { HOME_INDEX_MODULE };
