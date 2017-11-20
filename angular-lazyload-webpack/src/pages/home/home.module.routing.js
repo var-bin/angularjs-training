626171f2366a87714e0a53d5593f76abb909ac4f
var-bin <var-bin@users.noreply.github.com>
2017-11-20 13:59:31 +0200

    Refactoring. Using predefined states from home.module.states

--- angular-lazyload-webpack/src/pages/home/home.module.routing.js
+++ angular-lazyload-webpack/src/pages/home/home.module.routing.js
@@ -1,42 +1,17 @@
+// home.module.routing.js
+
 "use strict";
 
-const angular = require("angular");
+import { angular } from "angular";
+import { homeIndex, homeAbout } from "./home.module.states";
+
+homeRouting.$inject = ["$urlRouterProvider", "$stateProvider"];
 
 function homeRouting($urlRouterProvider, $stateProvider) {
   $urlRouterProvider.otherwise("/home");
 
-  $stateProvider
-    .state("home", {
-      url: "/home",
-      component: "homeComponent",
-      lazyLoad: ($transition$) => {
-        const $ocLazyLoad = $transition$.injector().get("$ocLazyLoad");
-
-        return new Promise((resolve, reject) => {
-          require.ensure([], () => {
-            // load whole module
-            const module = require("./home.module");
-
-            $ocLazyLoad.load({
-              name: "homeModule"
-            });
-
-            if (module.HOME_MODULE) {
-              resolve(module.HOME_MODULE);
-            } else {
-              reject("Ooops, somethig went wrong!");
-            }
-          });
-        });
-      }
-    })
-
-    .state("home.about", {
-      url: "/about",
-      component: "homeAboutComponent"
-    });
+  $stateProvider.state(homeIndex);
+  $stateProvider.state(homeAbout);
 }
 
-export default angular
-  .module("home.module.routing", [])
-  .config(homeRouting);
+export { homeRouting };
