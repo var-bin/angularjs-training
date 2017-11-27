ace1f805a1356b1147610b8972c1eece78720d0f
var-bin <var-bin@users.noreply.github.com>
2017-11-27 10:40:21 +0200

    Added BLOG_MODULE as a depemdency for lazyApp

--- angular-lazyload-webpack/src/app.js
+++ angular-lazyload-webpack/src/app.js
@@ -6,13 +6,15 @@ import angular from "angular";
 import "@uirouter/angularjs";
 import "oclazyload";
 
-import { HOME_MODULE } from "./pages/home/home.module"
+import { HOME_MODULE } from "./pages/home/home.module";
+import { BLOG_MODULE } from "./pages/blog/blog.module";
 
 const LAZY_APP =  angular
   .module("lazyApp", [
     "ui.router",
     "oc.lazyLoad",
-    "homeModule"
+    "homeModule",
+    BLOG_MODULE.name
   ]);
 
 export { LAZY_APP };
