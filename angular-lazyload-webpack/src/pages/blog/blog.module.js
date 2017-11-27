af335381f1f0fa317382b329b2c185bf43a432fe
var-bin <var-bin@users.noreply.github.com>
2017-11-27 13:44:05 +0200

    Using BlogService

--- angular-lazyload-webpack/src/pages/blog/blog.module.js
+++ angular-lazyload-webpack/src/pages/blog/blog.module.js
@@ -1,12 +1,26 @@
 // blog.module.js
 
 import { blogRouting } from "./blog.module.routing";
+import { BlogService } from "./blog.service";
 
 class BlogController {
-  constructor() {
+  constructor(BlogService) {
     this.title = "BlogComponent";
+    this.BlogService = BlogService;
+    this.repos = [];
+
+    this.getArticles();
+  }
+
+  getArticles() {
+    this.BlogService.getData()
+      .then((data) => {
+        this.repos = data;
+      });
   }
 }
+
+BlogController.$inject = ["BlogService"];
 
 class BlogComponent {
   constructor() {
@@ -18,6 +32,7 @@ class BlogComponent {
 const BLOG_MODULE = angular
   .module("blog.module", [])
   .component("blogComponent", new BlogComponent())
+  .service("BlogService", BlogService)
   .config(blogRouting);
 
 export { BLOG_MODULE };
