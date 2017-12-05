9a25e6a1a734a63eff00839420cbce2b22be1089
var-bin <var-bin@users.noreply.github.com>
2017-12-05 22:50:28 +0200

    Edit ensure definition

--- angularjs-lazyload-ts-webpack/configuration/declarations/require.d.ts
+++ angularjs-lazyload-ts-webpack/configuration/declarations/require.d.ts
@@ -1,3 +1,5 @@
 interface NodeRequire {
-  ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
+  <T>(path: string): T;
+  (paths: string[], callback: (...modules: any[]) => void): void;
+  ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void, name: string) => void;
 }
