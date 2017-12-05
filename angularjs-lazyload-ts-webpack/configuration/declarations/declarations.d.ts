066972eb9bcb06dc227148dc9863b8b87d712b76
var-bin <var-bin@users.noreply.github.com>
2017-12-05 18:57:35 +0200

    Delete useless file

--- angularjs-lazyload-ts-webpack/configuration/declarations/declarations.d.ts
+++
@@ -1,6 +0,0 @@
-declare const SB_ENV: string;
-declare const BUILD_TYPE: string;
-declare const SB_URL_ENV: string;
-declare const APP_CONFIG: string;
-declare let pdfMake: any;
-declare let moment: any;
