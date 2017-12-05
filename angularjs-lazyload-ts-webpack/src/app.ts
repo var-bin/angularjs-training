4c6b0b50a2b52a8a2d27b9373bda8a10491aeabf
var-bin <var-bin@users.noreply.github.com>
2017-12-05 22:51:15 +0200

    Added homeModule as a dependency for lazyApp module

--- angularjs-lazyload-ts-webpack/src/app.ts
+++ angularjs-lazyload-ts-webpack/src/app.ts
@@ -5,11 +5,13 @@
 import * as angular from "angular";
 import "@uirouter/angularjs";
 import "oclazyload";
+import "./pages/home/home.module";
 
 const LAZY_APP =  angular
   .module("lazyApp", [
     "ui.router",
-    "oc.lazyLoad"
+    "oc.lazyLoad",
+    "homeModule"
   ]);
 
 export { LAZY_APP };
