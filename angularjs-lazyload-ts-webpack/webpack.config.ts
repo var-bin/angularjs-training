5f5f178e12f12272869ac3321ba7c7f44684d799
var-bin <var-bin@users.noreply.github.com>
2017-12-06 14:22:02 +0200

    Edit webpack config

--- angularjs-lazyload-ts-webpack/webpack.config.ts
+++ angularjs-lazyload-ts-webpack/webpack.config.ts
@@ -5,109 +5,142 @@ import * as path from "path";
 import * as CleanWebpackPlugin from "clean-webpack-plugin";
 import * as HtmlWebpackPlugin from "html-webpack-plugin";
 import * as ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import * as UglifyJsPlugin from "uglifyjs-webpack-plugin";
 
-const config: webpack.Configuration = {
-  entry: {
-    app: "./src/core/bootstrap.ts"
-  },
+const devServer =  {
+  contentBase: path.resolve("dist"),
+  hot: true,
+  host: "localhost",
+  port: 8080
+};
 
-  output: {
-    filename: "bundle.js",
-    chunkFilename: "[name].bundle.js",
-    path: path.resolve(__dirname + "dist")
-  },
+const SourceMapDevToolPluginOptions = {
+  filename: "[name].js.map"
+};
 
-  resolve: {
-    extensions: [
-      ".ts",
-      ".js"
-    ]
-  },
+interface IEnv {
+  development: boolean;
+  production: boolean;
+}
 
-  module: {
-    rules: [
-      /*** transpile (typescript) ***/
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        loader: "ts-loader",
-        options: {
-          happyPackMode: true,
-          // disable type checker - we will use it in fork plugin
-          transpileOnly: true
+const config = (env: IEnv) => {
+  const options: webpack.Configuration | any = {
+    entry: {
+      app: "./src/core/bootstrap.ts"
+    },
+
+    output: {
+      filename: "bundle.js",
+      chunkFilename: "[name].bundle.js",
+      path: path.join(__dirname, "dist")
+    },
+
+    resolve: {
+      extensions: [
+        ".ts",
+        ".js"
+      ]
+    },
+
+    module: {
+      rules: [
+        /*** transpile (typescript) ***/
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          loader: "ts-loader",
+          options: {
+            happyPackMode: true,
+            // disable type checker - we will use it in fork plugin
+            transpileOnly: true
+          }
+        },
+
+        /*** TSLint check ***/
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          enforce: "pre",
+          loader: "tslint-loader",
+          options: {
+            // tslint errors are displayed by default as warnings
+            // set emitErrors to true to display them as errors
+            emitErrors: false,
+
+            // tslint does not interrupt the compilation by default
+            // if you want any file with tslint errors to fail
+            // set failOnHint to true
+            failOnHint: false
+          }
+        },
+
+        /*** css ***/
+        {
+          test: /\.css$/,
+          use: [
+            "style-loader",
+            "css-loader"
+          ]
+        },
+
+        /*** html ***/
+        {
+          test: /\.html$/,
+          loader: "ng-cache-loader?prefix=[dir]/[dir]",
+          exclude: path.join(__dirname, "src", "index.html")
         }
-      },
+      ]
+    },
 
-      /*** TSLint check ***/
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        enforce: "pre",
-        loader: "tslint-loader",
-        options: {
-          // tslint errors are displayed by default as warnings
-          // set emitErrors to true to display them as errors
-          emitErrors: false,
+    plugins: [
+      new CleanWebpackPlugin([path.join(__dirname, "dist")], {
+        root: __dirname
+      }),
 
-          // tslint does not interrupt the compilation by default
-          // if you want any file with tslint errors to fail
-          // set failOnHint to true
-          failOnHint: false
+      new ForkTsCheckerWebpackPlugin(),
+
+      new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./src/index.html",
+        hash: true
+      }),
+
+      new webpack.optimize.CommonsChunkPlugin({
+        name: "common",
+        filename: "common.js",
+        minChunks: (module) => {
+          // this assumes your vendor imports exist in the node_modules directory
+          return module.context && module.context.indexOf("node_modules") !== -1;
         }
-      },
+      }),
 
-      /*** css ***/
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
-      },
-
-      /*** html ***/
-      {
-        test: /\.html$/,
-        loader: "ng-cache-loader?prefix=[dir]/[dir]",
-        exclude: path.join(__dirname, "src", "index.html")
-      }
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin()
     ]
-  },
+  };
 
-  plugins: [
-    new CleanWebpackPlugin([path.resolve(__dirname + "dist")], {
-      root: __dirname
-    }),
-
-    new ForkTsCheckerWebpackPlugin(),
-
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/index.html",
-      hash: true
-    }),
-
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "common",
-      filename: "common.js",
-      minChunks: (module) => {
-        // this assumes your vendor imports exist in the node_modules directory
-        return module.context && module.context.indexOf("node_modules") !== -1;
-      }
-    }),
-
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-
-  devtool: "#@cheap-eval-source-map",
-
-  devServer: {
-    contentBase: path.resolve("dist"),
-    hot: true,
-    host: "localhost",
-    port: 8080
+  if (env.development) {
+    options.devServer;
+    options.plugins.push(
+      new webpack.SourceMapDevToolPlugin(SourceMapDevToolPluginOptions)
+    );
   }
+
+  if (env.production) {
+    options.plugins.push(
+      new webpack.SourceMapDevToolPlugin(SourceMapDevToolPluginOptions),
+
+      new UglifyJsPlugin({
+        sourceMap: true,
+        uglifyOptions: {
+          ecma: 5,
+          warnings: true
+        }
+      })
+    );
+  }
+
+  return options;
 };
 
 export default config;
