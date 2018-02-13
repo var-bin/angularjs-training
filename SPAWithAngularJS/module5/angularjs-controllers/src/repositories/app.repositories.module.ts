ef930a47f0bfc01e649f9509fea5b95ffd1ff9b8
var-bin <var-bin@users.noreply.github.com>
2018-02-13 18:46:23 +0200

    Added naming export

--- SPAWithAngularJS/module5/angularjs-controllers/src/repositories/app.repositories.module.ts
+++ SPAWithAngularJS/module5/angularjs-controllers/src/repositories/app.repositories.module.ts
@@ -6,6 +6,8 @@ import * as angular from "angular";
 import { RepositoriesComponent } from "./app.repositories.component";
 import { RepositoriesService } from "./app.repositories.service";
 
-angular.module("myApp.repositories", [])
+const myApp_Repositories = angular.module("myApp.repositories", [])
   .component("repositoriesComponent", new RepositoriesComponent())
   .service("RepositoriesService", RepositoriesService);
+
+export { myApp_Repositories };
