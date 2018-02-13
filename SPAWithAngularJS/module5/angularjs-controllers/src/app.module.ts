594325cfd392c59aabf42d2f58bc0b82fbc9548b
var-bin <var-bin@users.noreply.github.com>
2018-02-13 18:46:07 +0200

    Refactoring: Added oclazyload for support lazyLoad modules

--- SPAWithAngularJS/module5/angularjs-controllers/src/app.module.ts
+++ SPAWithAngularJS/module5/angularjs-controllers/src/app.module.ts
@@ -5,8 +5,7 @@
 import * as angular from "angular";
 import "@uirouter/angularjs";
 import { Message1Component } from "./message1/app.message1.controller";
-
-import "./repositories/app.repositories.module";
+import "oclazyload";
 
 RoutesConfig.$inject = ["$stateProvider", "$urlRouterProvider"];
 
@@ -27,10 +26,19 @@ function RoutesConfig(
     url: "/"
   };
 
-  const repositoriesState: angular.ui.IState = {
+  const repositoriesState = {
     name: "repositories",
     url: "/repositories",
-    component: "repositoriesComponent"
+    component: "repositoriesComponent",
+    lazyLoad: ($transition$) => {
+      const $ocLazyLoad = $transition$.injector().get("$ocLazyLoad");
+
+      return import("./repositories/app.repositories.module")
+        .then(mod => $ocLazyLoad.load(mod.myApp_Repositories))
+        .catch(err => {
+          throw new Error("Ooops, something went wrong, " + err);
+        });
+    }
   };
 
   // Redirect to home if no other URL matches
@@ -41,6 +49,6 @@ function RoutesConfig(
   $stateProvider.state(repositoriesState);
 }
 
-angular.module("myApp", ["ui.router", "myApp.repositories"])
+angular.module("myApp", ["ui.router", "oc.lazyLoad"])
   .component("message1Component", new Message1Component())
   .config(RoutesConfig);
